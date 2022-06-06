99779be9eb035a729ab35841c3e48cd2e7150605
Metody Aleksov <teddy57603@gmail.com>
2022-06-06 13:19:33 +0300

    updated the main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var control = new TextBox();
+        control.Enable();
+
+        System.out.println(control.isEnabled());
     }
 }
