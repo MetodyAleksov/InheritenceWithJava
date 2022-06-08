629388b417edc087fd6c74d0edbd56eeef9d3339
Metody Aleksov <teddy57603@gmail.com>
2022-06-08 15:15:05 +0300

    overrode hashCode method in Point class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,8 @@ public class Main {
 	    var point1 = new Point(1, 2);
         var point2 = new Point(1, 2);
 
-        System.out.println(point1.equals(point2));
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
     }
 
     public static void show(UIControl control){
--- src/com/company/Point.java
+++ src/com/company/Point.java
@@ -13,15 +13,33 @@ public class Point {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o){
-            return true;
-        }
-
-        if (o == null || this.getClass() != o.getClass()) {
-            return false;
-        }
-
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Point point = (Point) o;
         return x == point.x && y == point.y;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    //    @Override
+//    public boolean equals(Object o) {
+//        if (this == o){
+//            return true;
+//        }
+//
+//        if (o == null || this.getClass() != o.getClass()) {
+//            return false;
+//        }
+//
+//        Point point = (Point) o;
+//        return x == point.x && y == point.y;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(this.x, this.y);
+//    }
 }
